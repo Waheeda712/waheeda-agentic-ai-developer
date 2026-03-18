330eabd818014c80a9f0855727c4d63d512dca7d
Waheeda <waheedakatike@gmail.com>
2026-03-18 22:59:56 +0530

    variables of python

---
+++ Basics/variables.py
@@ -0,0 +1,33 @@
+# num1 = 200
+# num2 = 100
+# num1=num2=200
+# add= num1+num2
+# num3= int
+# print("Addition :",add)
+# print(type(add))
+
+# age =20
+# condition = age <30
+# print(condition)
+
+# List
+
+# num = [10,2,3,5,6,7]
+# print(num[0],num[3])
+# print(num[2:4])
+# for number in num:
+#     print(number)
+
+# num = [10,20,30,40,50]
+# num[0] = 100
+# num.append(50)
+# num.insert(1,90)
+# print(num)
+
+#Tuples
+# num = (10,20,30,40,50,60)
+# list1 = list(num)
+# list1[2] = 400
+# num2 = tuple(list1)
+# print(num2)
+
