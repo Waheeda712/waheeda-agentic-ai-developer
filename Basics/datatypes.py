1afbc0989a5c33629b92a593f915ad4e8869e24f
Waheeda <waheedakatike@gmail.com>
2026-03-19 00:13:51 +0530

    datatypes of python

---
+++ Basics/datatypes.py
@@ -0,0 +1,18 @@
+# num = bytes([65,66,67])
+# print(num)
+
+# num = bytearray([65,66,67])
+# num[0] = 97
+# print(num)
+
+# Comparision
+# list1 = [100,200,300]
+# list2 = [100,200,300]
+# print(list1 == list2)
+# print(list1 is list2)
+
+#String
+name = "Waheeda"
+print(name[0], name[-2])
+# name[0] = "V" # we can't modify list because it is immutable
+# print(name)
