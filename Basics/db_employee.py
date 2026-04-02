e9454330fad4e1485cf8a36bc443936fa43490e2
Waheeda <waheedakatike@gmail.com>
2026-04-02 09:10:56 +0530

    pandas operation

--- Basics/db_employee.py
+++ Basics/db_employee.py
@@ -19,5 +19,20 @@ print("---------------------")
 # print(drop_row_based_on_salary)
 # print("-----------------------")
 
-drop_row_based_age_salary = db.dropna(subset=["age","Salary"])
-print(drop_row_based_age_salary)
+# drop_row_based_age_salary = db.dropna(subset=["age","Salary"])
+# print(drop_row_based_age_salary)
+# print("-----------------------")
+
+# fill_with_zero=db.fillna(0)
+# print(fill_with_zero)
+
+#db["age"] = 20 # print all the age with 20
+# db["age"] = db["age"].fillna(db["age"].mean()) # print the empty age with mean vaule
+# db["Salary"] = db["Salary"].fillna(db["Salary"].mean())
+# print(db)
+
+forward_fill = db.ffill()
+print(forward_fill) # fill the empty vaules with forward value
+print("-----------------------")
+backward_fill = db.bfill()
+print(backward_fill) # fill the empty values with backwardvaule
